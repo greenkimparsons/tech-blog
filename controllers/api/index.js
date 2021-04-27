2206cb7ac78c5312c26a92e429ca97916d346a18
Green Kim <greenkimparsons@gmail.com>
2021-04-26 22:09:30 -0400

    added delete btn but doesn't work yet

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./blogRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,9 +3,10 @@ const newFormHandler = async (event) => {
 
   const name = document.querySelector('#blog-name').value.trim();
   const description = document.querySelector('#blog-desc').value.trim();
-
+  console.log(name);
+  console.log(description);
   if (name && description) {
-    const response = await fetch(`/api/blogs`, {
+    const response = await fetch(`/api/blogs/`, {
       method: 'POST',
       body: JSON.stringify({ name, description }),
       headers: {
@@ -21,26 +22,26 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/blogs/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/api/blogs/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to delete blog');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/dashboard');
+//     } else {
+//       alert('Failed to delete blog');
+//     }
+//   }
+// };
 
 document
   .querySelector('.new-blog-form')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.blog-list')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('.blog-list')
+//   .addEventListener('click', delButtonHandler);
